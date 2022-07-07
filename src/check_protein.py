9079583ddc39c8ff05730f5964f5ab496052e69c
KinemuchiRyunosuke <kinemuchi1124@gmail.com>
2022-07-07 13:51:50 +0900

    b

--- src/check_protein.py
+++ src/check_protein.py
@@ -3,25 +3,26 @@ from Bio import SeqIO
 
 
 HIV_dict = {\
-    "HIV-1": {"gag": ["gag", "Gag"],
-              "pol": ["pol", "Pol"],
-              "vif": ["vif", "Vif", "viral infectivity factor"],
-              "vpr": ["vpr", "Vpr"],
-              "tat": ["tat", "Tat"],
-              "rev": ["rev", "Rev"],
-              "vpu": ["vpu", "Vpu"],
+    "HIV-1": {"gag": ["gag", "Gag", "GAG"],
+              "pol": ["pol", "Pol", "POL"],
+              "vif": ["vif", "Vif", "VIF", "viral infectivity factor"],
+              "vpr": ["vpr", "Vpr", "VPR", "vpR"],
+              "tat": ["tat", "Tat", "TAT"],
+              "rev": ["rev", "Rev", "REV"],
+              "vpu": ["vpu", "Vpu", "VPU"],
               "envelope glycoprotein":
-                ["env", "Env"],
-              "nef": ["nef", "Nef"]},
-    "HIV-2": {"gag": ["gag", "Gag"],
-              "vif": ["vif", "Vif"],
-              "vpx": ["vpx", "Vpx"],
-              "vpr": ["vpr", "Vpr"],
-              "tat": ["tat", "Tat"],
-              "rev": ["rev", "Rev"],
+                ["env", "Env", "gp120", "gp41", "gp160"],
+              "nef": ["nef", "Nef", "NEF"]},
+    "HIV-2": {"gag": ["gag", "Gag", "GAG"],
+              "pol": ["pol", "Pol", "POL"],
+              "vif": ["vif", "Vif", "VIF"],
+              "vpx": ["vpx", "Vpx", "VPX"],
+              "vpr": ["vpr", "Vpr", "VPR"],
+              "tat": ["tat", "Tat", "TAT"],
+              "rev": ["rev", "Rev", "REV"],
               "envelope polyprotein":
                 ["envelope polyprotein", "env protein"],
-              "nef": ["nef", "Nef"]}
+              "nef": ["nef", "Nef", "NEF"]}
 }
 
 
@@ -41,8 +42,10 @@ def main():
 def determine(desc, HIV_dict):
     # HIVの種別を判定
     kind, protein = None, None
-    keys_HIV1 = ["HIV-1", "Human immunodeficiency virus 1"]
-    keys_HIV2 = ["HIV-2", "Human immunodeficiency virus 2"]
+    keys_HIV1 = ["HIV-1", "Human immunodeficiency virus 1",
+                 "Human immunodeficiency virus type 1"]
+    keys_HIV2 = ["HIV-2", "Human immunodeficiency virus 2",
+                 "Human immunodeficiency virus type 2"]
 
     for key in keys_HIV1:
         if key in desc:
